5866144fc8d8d815990bbe24f5df06aa3672f520
Michel <woelfelm@t-online.de>
2023-10-07 18:23:52 +0200

    player design

--- ringoffire/src/models/game.ts
+++ ringoffire/src/models/game.ts
@@ -1,5 +1,5 @@
 export class Game {
-    public players: string[] = [];
+    public players: string[] = ['Hans', 'Michel', 'Basti'];
     public stack: string[] = [];
     public playedCard: string[] = [];
     public currentPlayer: number = 0;
